c68c2f612d92fa04c1e8bd1918027eee46dfa147
MelzEn <97205073+melsen888@users.noreply.github.com>
2022-09-29 21:09:38 +0700

    Add files via upload
    ++ upload file

---
+++ api/youtube.js
@@ -0,0 +1,1 @@
+const api_url = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c3f4093413bd8efd296d04f95cbf9ad4";
--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 const express = require('express');
+const upload = require('express-fileupload');
+// const mongoose = require('mongoose');
+const articlesRouter = require('./router/articles.ejs')
 var parseUrl = require('body-parser')
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 9999;
 const requestIp = require('request-ip'); // ip
 app.use(requestIp.mw())
 
 let encodeUrl = parseUrl.urlencoded({ extended: true })
 
+// upload
+app.use(upload())
+app.get('/upload', (req, res) => {
+    res.render(__dirname +'/views/project/upload.ejs')
+})
+app.post('/upload', (req, res) => {
+    if (req.files){
+        console.log(req.files)
+        var file = req.files.file
+        var filename = file.name
+        console.log(filename)
+
+        file.mv('./data/uploads/'+ filename,function(err){
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(`
+                <h3 style="text-center">File uploaded successfully</h3>
+                <a href="/data/uploads/${filename}">/data/uploads/${filename}</a>
+                `)
+            }
+        })
+    }
+})
+
 app.use('/public', express.static('public'))
+app.use('/data', express.static('data'))
+app.use('/articles', articlesRouter)
 
 // set view engine
 app.set('view engine', 'ejs', 'html');
 
 app.get ('/',(req, res) => {
-    res.render('index');
+    const articles = [{
+        title: 'Teori Asal Usul Nenek Moyang Indonesia',
+        createdAt: new Date(Date.now()),
+        description: 'teori asal usul nenek moyang Indonesia',
+    }]
+    res.render('index', {articles: articles});
 })
 
 // to set app get
@@ -31,6 +66,12 @@ app.get ('/alfa',(req, res) => {
 })
 
 app.get ('/melsen',(req, res, next) => {
+    res.render(__dirname + "/views/melsen/index.ejs");
+})
+
+// TODO: add helper, project , menu , comments , sql
+
+app.get ('/melsen/form',(req, res, next) => {
     const ip = req.clientIp;
     console.log(`Get /melsen \n ip : ${ip}
         `)
@@ -49,6 +90,10 @@ app.post('/melsen', encodeUrl, (req, res) => {
     res.end(ip);
 })
 
+app.get ('/getstarted',(req, res) => {
+    res.render(__dirname + "/router/getstarted.ejs");
+})
+
 app.post('/kenalan', encodeUrl, (req, res) => {
     const ip = req.clientIp;
     var today = new Date();
@@ -61,7 +106,12 @@ app.post('/kenalan', encodeUrl, (req, res) => {
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(404).send('hem... 500 error: ' + err.stack)
+    res.status(404).send('<h1 style="background-color: black; color: white; text-align: center;">ERROR 404 | Melsen</h1>' + err.stack )
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
 
 app.listen(port)
